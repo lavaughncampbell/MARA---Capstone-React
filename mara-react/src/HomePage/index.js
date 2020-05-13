375801aa0225a2203fe88f4a4c6bb04c7dcf5275
“lavaughncampbell” <lavaughnccampbell@gmail.com>
2020-05-13 15:16:51 -0500

    working on home page

---
+++ mara-react/src/HomePage/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+export default class Loading extends React.Component{
+	constructor(props) {
+		super(props); 
+	}
+
+
+	render() {
+		return(
+			<div>
+				<h2>Hello</h2>
+			</div>
+		)
+	}
+
+}
--- mara-react/src/components/loading.js
+++ mara-react/src/components/loading.js
@@ -3,33 +3,77 @@ import FadeIn from "react-fade-in";
 import Lottie from "react-lottie";
 import ReactLoading from "react-loading";
 import "bootstrap/dist/css/bootstrap.css";
+import '../App.css'; 
+import * as legoData from "../legoloading.json";
+import * as doneData from "../doneloading.json";
+import HomePage from '../HomePage'; 
+
+
+const defaultOptions = {
+loop: true,
+autoplay: true,
+animationData: legoData.default,
+rendererSettings: {
+	preserveAspectRatio: "xMidYMid slice"
+}
+};
+
+const defaultOptions2 = {
+  loop: false,
+  autoplay: true,
+  animationData: doneData.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
 
 export default class Loading extends React.Component {
 	constructor(props) {
 		super(props); 
 		this.state = {
+			loading: undefined,
 			done: undefined
 		}; 
 	}
+
 
 
 	componentDidMount() {
 		setTimeout(() => {
 			fetch("https://jsonplaceholder.typicode.com/posts")
 			.then(response => response.json())
-			.then(json => this.setState({ done: true })); 
-		}, 1200); 
+			.then(json => {
+				this.setState({ loading: true });
+				setTimeout(() => {
+					this.setState({ done: true });
+				}, 1000);
+			});
+		}, 1200);
 	}
 
 	render() {
     return (
       <div>
         {!this.state.done ? (
-          <ReactLoading type={"bars"} color={"white"} />
+          <FadeIn>
+            <div class="d-flex justify-content-center align-items-center">
+              <h1>fetching MARA</h1>
+              {!this.state.loading ? (
+                <Lottie options={defaultOptions} height={120} width={120} />
+              ) : (
+                <Lottie options={defaultOptions2} height={120} width={120} />
+              )}
+            </div>
+          </FadeIn>
         ) : (
-          <h1>MARA</h1>
+          <HomePage />
         )}
       </div>
     );
   }
 }
+
+
+
+
+			
